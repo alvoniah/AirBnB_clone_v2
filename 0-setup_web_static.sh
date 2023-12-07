65951b9e9251da766095f7419c438465e3a1f802
ONGORO <jumaongoro5@gmail.com>
2023-12-07 15:22:47 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
 FAKEHTMLFILE="/data/web_static/releases/test/index.html"
-echo "Let this be our prayer" | sudo tee "$FAKEHTMLFILE"
+echo "This is  a simple content" | sudo tee "$FAKEHTMLFILE"
 rm -f "/data/web_static/current"
 sudo rm -f "/data/web_static/releases/test/current"
 sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"

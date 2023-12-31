c64b48307d93c0e689741defd1284b3328f3f57d
alvoniah <alvoniah24@gmail.com>
2023-12-31 08:00:48 -0800

    flask

--- models/state.py
+++ models/state.py
@@ -1,64 +1,39 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-from models.city import City
-from models.base_model import Base
+"""Defines the State class."""
+import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
 from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """Represents a state for a MySQL database.
 
-    if (getenv("HBNB_TYPE_STORAGE") == 'db'):
-        name = Column(String(128), nullable=False)
-        # cities = relationship('City')
-        cities = relationship('City', cascade="all, delete", backref='state')
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         @property
         def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
+            """Get a list of City instances with
+                state_id equals to the current State.id.
 
+            This is a getter attribute for FileStorage
+                relationship between State and City.
             """
-            cities = storage.all(City)
-            # gets the entire storage- a dictionary
-            for key, value in cities.items():
-                # cities.value returns list of the city objects
-                if value.state_id == self.id:
-                    # if the object.state_id == self.id
-                    related_cities.append(value)
-                    # append to the cities list
-            return related_cities
-            """
-
-            '''
-            cities = storage.all(City).items()
-            # gets the entire storage- a dictionary
-            for city in cities.values():
-                    # cities.value returns list of the city objects
+            city_list = []
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
-                        # if the object.state_id == self.id
-                    related_cities.append(city)
-                    # append to the cities list
-            return related_cities
-                # state = relationship('State')
-                #the above block of code is buggy because,
-                we use the items() on 43
-                #which returns a dict with a key and
-                value pair but we return just the
-                #key and do not return the value of the key
-            '''
-
-            # another implementation of lines 43 to 47
-            cities = storage.all(City)
-            for key in cities.keys():
-                if cities[key].state_id == self.id:
-                    related_cities.append(cities[key])
-            return related_cities
+                    city_list.append(city)
+            return city_list
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""script starts a flask application"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-
-
 app = Flask(__name__)
-"""flask app"""
 
 
 @app.route('/states_list', strict_slashes=False)
-def get_list():
-    """displays a list of all states"""
-    states = storage.all('State')
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """Method close the db session"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
